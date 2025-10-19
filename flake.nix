956116dd0d251c5b48a210ba73c892d7161cf6ec
soymou <emilio.junoy@gmail.com>
2025-10-19 03:52:24 -0600

    Try to fix downgrading quickshell

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     
     quickshell = {
-      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell?rev=00858812f25b748d08b075a0d284093685fa3ffd";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
